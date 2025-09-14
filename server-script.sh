1b8fec97353bec56d37de53a88529d0038412671
AbhishekSaharawat <165172776+AbhishekSaharawat@users.noreply.github.com>
2025-09-14 23:41:04 +0530

    Update server-script.sh

--- server-script.sh
+++ server-script.sh
@@ -5,11 +5,11 @@ sudo apt install maven -y
 if [-d "addressbook-v1"]
 then
   echo "repo is clone and exists"
-  cd /home/ec2-user/addressbook-v1
+  cd /home/ubuntu/addressbook-v1
   git pull origin main
 else
  git clone https://github.com/AbhishekSaharawat/addressbook-v1.git
 fi
 
-cd /home/ec2-user/addressbook-v1
+cd /home/ubuntu/addressbook-v1
 mvn package
